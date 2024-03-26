4dcb50afd5b9b9b274d43ebc8ad0368ff1e999e5
Kyle <kyle.treoxer@gmail.com>
2024-03-26 21:15:12 +0700

    feat: delete transaction logs on user deletion

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,8 +5,12 @@ from api.models import User, TransactionLog
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-
     list_display = [field.name for field in User._meta.fields]
+
+    # on delete, remove all related transaction logs
+    def delete_model(self, request, obj):
+        TransactionLog.objects.filter(uid=obj.uid).delete()
+        obj.delete()
 
 
 @admin.register(TransactionLog)
